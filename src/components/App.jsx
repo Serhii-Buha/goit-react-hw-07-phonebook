8bded875269ede5fa4431bc11b0e33babe16ee61
Serhii-Buha <serhii.buga@gmail.com>
2023-06-14 11:23:01 +0300

    add useEffect, InfinitySpin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
-import Container from 'react-bootstrap/Container';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { InfinitySpin } from 'react-loader-spinner';
+import { fetchContacts } from 'redux/users/operations';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { getError, getIsLoading } from 'redux/users/userSelectors';
+import { ContainerCustom, SpinStyle } from './ContactForm/ContactForm.styled';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <Container>
+    <ContainerCustom>
       <h1>Phonebook</h1>
       <ContactForm />
 
@@ -13,6 +26,10 @@ export const App = () => {
       <Filter />
 
       <ContactList />
-    </Container>
+
+      <SpinStyle>
+        {isLoading && !error && <InfinitySpin color="#4c2ef7" />}
+      </SpinStyle>
+    </ContainerCustom>
   );
 };
