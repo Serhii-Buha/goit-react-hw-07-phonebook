424e2afca7c5a6d71dbe5ec804dee43fad6b2175
Serhii-Buha <serhii.buga@gmail.com>
2023-06-14 11:20:55 +0300

    delete redux-persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'redux/store';
 import { App } from 'components/App';
 
 import './index.css';
@@ -11,9 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
