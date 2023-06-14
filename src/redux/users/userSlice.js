d31edfcfe553c9904aa8804cfb5d5f1ff7042a61
Serhii-Buha <serhii.buga@gmail.com>
2023-06-14 11:25:56 +0300

    add extraReducers, addCase, operations

--- src/redux/users/userSlice.js
+++ src/redux/users/userSlice.js
@@ -1,21 +1,53 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  addContact,
+  deleteContact,
+  fetchContacts,
+} from 'redux/users/operations';
 
 const initialState = {
-  users: [],
+  items: [],
+  isLoading: false,
+  error: null,
 };
 
 export const userSlice = createSlice({
-  name: 'users',
+  name: 'contacts',
   initialState,
-  reducers: {
-    addUser: (state, { payload }) => {
-      state.users.push(payload);
-    },
-    deleteUser: (state, { payload }) => {
-      state.users = state.users.filter(item => item.id !== payload);
-    },
+
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => (state.isLoading = true))
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.items = payload;
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(fetchContacts.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(addContact.pending, state => (state.isLoading = true))
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.items.push(payload);
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(addContact.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(deleteContact.pending, state => (state.isLoading = true))
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        state.items = state.items.filter(contact => contact.id !== payload.id);
+        state.error = null;
+        state.isLoading = false;
+      })
+      .addCase(deleteContact.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      });
   },
 });
 
-export const { addUser, deleteUser } = userSlice.actions;
 export const userReducer = userSlice.reducer;
